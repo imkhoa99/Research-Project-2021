7e0f27991c01ceaf559ea1b0574d8893bbf23eeb
tep0601 <43793333+tep0601@users.noreply.github.com>
2021-12-06 18:24:04 +0200

    add

--- our_work/jukebox/main.py
+++ our_work/jukebox/main.py
@@ -5,3 +5,16 @@ import torch
 
 JUKEBOX_SAMPLE_RATE = 44100
 
+def load_audio_from_file(fpath):
+    audio, _ = lr.load(fpath, sr=JUKEBOX_SAMPLE_RATE)
+    if audio.ndim == 1:
+        audio = audio[np.newaxis]
+    audio = audio.mean(axis=0)
+
+    # normalize audio
+    norm_factor = np.abs(audio).max()
+    if norm_factor > 0:
+        audio /= norm_factor
+
+    return audio.flatten()
+    
